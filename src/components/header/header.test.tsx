87202fbfea749206cd17d545c033e06c06c9106d
ezedelriodev <eze.delrio.dev@gmail.com>
2024-10-23 09:49:50 +0200

    feat: [PODCAST] create configuration for testing with vitest

---
+++ src/components/header/header.test.tsx
@@ -0,0 +1,14 @@
+
+import { render, screen } from '@testing-library/react';
+import { describe, it, expect } from 'vitest';
+import Header from './header';
+
+
+describe('Header component', () => {
+    it('should render the text "Podcaster"', () => {
+      render(<Header />);
+      
+      const headerElement = screen.getByText(/Podcaster/i);
+      expect(headerElement).toBeInTheDocument();
+    });
+  });
