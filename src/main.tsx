a5f6d97c8ecefce2b638e0e066cb0e831e06d8d0
ezedelriodev <eze.delrio.dev@gmail.com>
2024-10-29 08:14:53 +0100

    refactor: [PODCAST] Disable ReactQueryDevtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { BrowserRouter } from "react-router-dom";
 import App from './App.tsx'
 import './index.css'
@@ -9,7 +8,6 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
-    <ReactQueryDevtools />
       <BrowserRouter>
         <App />
       </BrowserRouter>
